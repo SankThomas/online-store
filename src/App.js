586257106422c4f0916919e289c8c7a6bf2a3c5e
Thomas Sankara <thomassankara74@gmail.com>
2021-01-22 00:41:33 +0300

    display images and data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import Products from './pages/home/Products'
+import Heels from './pages/heels/Heels'
+import Dresses from './pages/dresses/Dresses'
+import Fashion from './pages/fashion/Fashion'
+import Shoes from './pages/shoes/Shoes'
+import Shorts from './pages/shorts/Shorts'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+
+      <Switch>
+        <Route path="/" exact>
+          <Products />
+        </Route>
+        <Route path="/heels">
+          <Heels />
+        </Route>
+        <Route path="/dresses">
+          <Dresses />
+        </Route>
+        <Route path="/fashion">
+          <Fashion />
+        </Route>
+        <Route path="/shoes">
+          <Shoes />
+        </Route>
+        <Route path="/shorts">
+          <Shorts />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <>
+      <nav className="bg-green-600 py-4 text-white">
+        <ul className="flex justify-center">
+          <li className="mx-2 tracking-widest uppercase font-bold">
+            <NavLink to="/" className="transition-colors hover:text-green-900">
+              Home
+            </NavLink>
+          </li>
+          <li className="mx-2 tracking-widest uppercase font-bold">
+            <NavLink
+              to="/heels"
+              className="transition-colors hover:text-green-900"
+            >
+              Heels
+            </NavLink>
+          </li>
+          <li className="mx-2 tracking-widest uppercase font-bold">
+            <NavLink
+              to="/dresses"
+              className="transition-colors hover:text-green-900"
+            >
+              Dresses
+            </NavLink>
+          </li>
+          <li className="mx-2 tracking-widest uppercase font-bold">
+            <NavLink
+              to="/fashion"
+              className="transition-colors hover:text-green-900"
+            >
+              Fashion
+            </NavLink>
+          </li>
+          <li className="mx-2 tracking-widest uppercase font-bold">
+            <NavLink
+              to="/shoes"
+              className="transition-colors hover:text-green-900"
+            >
+              Shoes
+            </NavLink>
+          </li>
+          <li className="mx-2 tracking-widest uppercase font-bold">
+            <NavLink
+              to="/shorts"
+              className="transition-colors hover:text-green-900"
+            >
+              Shorts
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Header
