02dc5d0e7acbdf73aaf104ce81005786fde6ad2c
Thomas Sankara <thomassankara74@gmail.com>
2021-01-22 00:45:48 +0300

    add flex wrap on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <nav className="bg-green-600 py-4 text-white">
-        <ul className="flex justify-center">
+        <ul className="flex flex-wrap justify-center">
           <li className="mx-2 tracking-widest uppercase font-bold">
             <NavLink to="/" className="transition-colors hover:text-green-900">
               Home
